da615605c44c3ab0b40b27d9203f3d70d7131222
leegao <lg342@cornell.edu>
2026-07-10 00:27:04 +0800

    Add push harness for testing

---
+++ push.sh
@@ -0,0 +1,18 @@
+push() {
+	adb push $1 /data/local/tmp/
+	adb shell run-as com.ludashi.benchmark "cp /data/local/tmp/$1 files/imagefs/usr/lib/$1"
+	adb shell run-as com.ludashi.benchmark "ls -lh files/imagefs/usr/lib/$1"
+}
+
+push_vvl() {
+	adb push $1 /data/local/tmp/
+	adb shell run-as com.ludashi.benchmark "cp /data/local/tmp/$1 files/imagefs/usr/share/vulkan/explicit_layer.d/$1"
+	adb shell run-as com.ludashi.benchmark "ls -lh files/imagefs/usr/share/vulkan/explicit_layer.d/$1"
+}
+
+sh compile_android.sh
+cd build_arm64
+push libdxvk_mali_compat_layer.so
+cd -
+
+push_vvl libdxvk_mali_compat_layer.json
